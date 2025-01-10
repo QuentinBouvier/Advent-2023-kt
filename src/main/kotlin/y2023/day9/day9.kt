7d3d50ff62cf14606abe431852a65bfdf09df5a3
Quentin Bouvier <32564163+QuentinBouvier@users.noreply.github.com>
2025-01-11 00:33:49 +0100

    day 9 part 2

--- src/main/kotlin/y2023/day9/day9.kt
+++ src/main/kotlin/y2023/day9/day9.kt
@@ -10,19 +10,33 @@ fun main() {
 //10 13 16 21 30 45""".split("\n")
 
     part1(input)
+    part2(input)
 }
 
 fun part1(input: List<String>) {
     val p1 = input.fold(0) { result, line ->
-        val matrix: MutableList<List<Int>> = mutableListOf()
-        var l = line.split(" ").map { it.toInt() }
-        while (!l.all { it == 0 }) {
-            matrix.add(l)
-            l = addWithNext(l)
-        }
+        val matrix = createMatrix(line)
         result + matrix.fold(0) { t, it -> t + it.last() }
     }
     println(p1)
+}
+
+fun part2(input: List<String>) {
+    val p2 = input.map { line ->
+        val matrix = createMatrix(line)
+        matrix.reversed().fold(0) { t, it -> it.first() - t}
+    }
+    print(p2.sum())
+}
+
+private fun createMatrix(line: String): List<List<Int>> {
+    val matrix: MutableList<List<Int>> = mutableListOf()
+    var l = line.split(" ").map { it.toInt() }
+    while (!l.all { it == 0 }) {
+        matrix.add(l)
+        l = addWithNext(l)
+    }
+    return matrix
 }
 
 fun addWithNext(l: List<Int>) = l.zipWithNext().map { it.second - it.first }
